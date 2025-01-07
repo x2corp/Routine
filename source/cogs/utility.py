15a27375d1b040904aadc80d03f82d4b50bbacd7
Elle <elle@uq.ci>
2025-01-07 15:19:55 +0000

    Full framework rewrite

---
+++ source/cogs/utility.py
@@ -0,0 +1,12 @@
+from discord.ext import commands
+
+class Utility(commands.Cog):
+    def __init__(self, bot):
+        self.bot: commands.AutoShardedBot = bot
+    
+    @commands.command(pass_context=True)
+    async def latency(self, ctx):
+        await ctx.send('Fully returned at {0}.'.format(round(self.bot.latency, 1)))
+
+async def setup(bot: commands.AutoShardedBot):
+    await bot.add_cog(Utility(bot))
--- source/core/bot.py
+++ source/core/bot.py
@@ -1,8 +1,45 @@
 from discord.ext import commands
+from logging import handlers, Formatter
+from .. import cogs
+import logging, yaml, os, discord
 
-class Routine(commands.AutoShardedBot):
+with open("config.yml", "r") as file:
+    config = yaml.load(file, Loader=yaml.FullLoader)
+            
+class Routine(commands.Bot):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
     
+    async def setup_hook(self) -> None:
+        for extension in os.listdir("./source/cogs"):
+          if extension.endswith(".py"):
+              await self.load_extension(f"source.cogs.{extension[:-3]}")
+
+        logger = logging.getLogger('main')
+        logger.setLevel(logging.WARN)
+
+        handler = handlers.BaseRotatingHandler(
+            filename='Routine.log',
+            mode='a',
+            delay=True
+        )
+
+        datetime_format = '%d/%m/%Y %H:%M'
+        formatter = Formatter('[{asctime} | {levelname}] {name}: {message}', datetime_format, style='{')
+        handler.setFormatter(formatter)
+        logger.addHandler(handler)
+        
     async def on_ready(self):
-        print("Bot is up on", self.user.name)
+        print("Bot is running on user {0}, returned in {1}ms.".format(
+            self.user.name, 
+            round(self.latency, 1)
+        ))
+
+async def create_client() -> None:
+    async with Routine(
+        command_prefix = config['prefix'],
+        description = "Akatsuki-focused Discord bot",
+        intents = discord.Intents.all()
+    ) as bot:
+        await bot.start(config["token"])
+
